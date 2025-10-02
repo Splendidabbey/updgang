06512e24e01fea2ce43284e1482125380ff1bd01
Abiodun Olorunda <splendidabbey@gmail.com>
2025-10-02 19:12:01 +0100

    Add complete Composer autoloader structure for XenForo

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'xenforo/forum',
+        'pretty_version' => 'dev-master',
+        'version' => 'dev-master',
+        'reference' => 'c476e6ad0ef9ac3637e9f44fcfc50e5b7df66d1c',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'xenforo/forum' => array(
+            'pretty_version' => 'dev-master',
+            'version' => 'dev-master',
+            'reference' => 'c476e6ad0ef9ac3637e9f44fcfc50e5b7df66d1c',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
