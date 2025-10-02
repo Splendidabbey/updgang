8a87aec9486df5b8770b3b471622157b615e3c5b
Abiodun Olorunda <splendidabbey@gmail.com>
2025-10-02 18:28:39 +0100

    Simplify autoloader to minimal XenForo-compatible version

--- src/vendor/autoload.php
+++ src/vendor/autoload.php
@@ -1,29 +1,21 @@
 <?php
 /**
  * XenForo Autoloader
- * Simple autoloader for XenForo deployment
+ * Minimal autoloader for XenForo deployment
  */
 
-// Only register if not already registered
-if (!function_exists('xf_autoloader')) {
-    function xf_autoloader($class) {
-        // Convert namespace to file path
-        $class = str_replace('\\', '/', $class);
-        
-        // Handle XF namespace
-        if (strpos($class, 'XF/') === 0) {
-            $class = substr($class, 3);
-            $file = __DIR__ . '/../XF/' . $class . '.php';
-            
-            if (file_exists($file)) {
-                require_once $file;
-                return true;
-            }
-        }
-        
-        return false;
-    }
+// Simple autoloader that XenForo can work with
+spl_autoload_register(function ($class) {
+    // Convert namespace to file path
+    $class = str_replace('\\', '/', $class);
     
-    // Register the autoloader
-    spl_autoload_register('xf_autoloader');
-}
+    // Handle XF namespace
+    if (strpos($class, 'XF/') === 0) {
+        $class = substr($class, 3);
+        $file = __DIR__ . '/../XF/' . $class . '.php';
+        
+        if (file_exists($file)) {
+            require_once $file;
+        }
+    }
+});
